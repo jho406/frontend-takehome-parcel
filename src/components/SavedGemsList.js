2df0a65a0a1b668f3834f989824a2021c1b1ddfa
johny <jho406@gmail.com>
2019-09-28 15:19:27 -0400

    Add SavedGemList component

---
+++ src/components/SavedGemsList.js
@@ -0,0 +1,34 @@
+import React from 'react'
+
+export default class SavedGemsList extends React.Component {
+  static defaultProps = {
+    onRemoveClick: ()=>{},
+    items: [],
+  }
+
+  render() {
+    const onRemoveClick = this.props.onRemoveClick
+    const hasEmptyItems = this.props.items.length === 0
+    const savedSearchElements = this.props.items.map((gem) => {
+      return (
+        <div className='saved-gems__item' key={gem.id}>
+          {gem.name}
+          <a className='saved-gems__item__remove-link' onClick={() => {this.props.onRemoveClick(gem.id)}} href='#'>remove</a>
+          <div className='saved-gems__item__description'>{gem.description}</div>
+        </div>
+      )
+    })
+
+    return (
+      <div className="card saved-gems">
+        <div>Saved searches</div>
+        { hasEmptyItems ?
+          <div className='saved-gems__empty-notice'>
+            There are no items saved yet.
+          </div> : null
+        }
+        {savedSearchElements}
+      </div>
+    )
+  }
+}
---
+++ src/components/SavedGemsListSpec.js
@@ -0,0 +1,55 @@
+import '../../spec/support/enzyme'
+import 'jsdom-global/register'
+import { mount } from 'enzyme'
+import React from 'React'
+import sinon from 'sinon'
+import jasmineEnzyme from 'jasmine-enzyme'
+import SavedGemsList from './SavedGemsList'
+
+describe('SavedGemsList', () => {
+  describe('render', () => {
+    it('renders an empty message when no items are passed', () => {
+      const handle = '.saved-gems__empty-notice'
+      const expectedMsg = 'There are no items saved yet.'
+      const items = [
+        {id: 'foo', name: 'rails', description: 'rails'}
+      ]
+
+      let wrapper = mount(<SavedGemsList/>)
+      expect(wrapper.find(handle)).toHaveText(expectedMsg)
+
+      wrapper = mount(<SavedGemsList items={items}/>)
+      expect(wrapper.find(handle)).not.toHaveText(expectedMsg)
+    })
+
+    it('renders a list of gems when items are passed', () => {
+      const handle = '.saved-gems__item'
+      const items = [
+        {id: 'foo', name: 'rails', description: 'rails'},
+        {id: 'bar', name: 'sprok', description: 'sprok'}
+      ]
+
+      const wrapper = mount(<SavedGemsList items={items}/>)
+
+      expect(wrapper).toContainMatchingElements(2, handle)
+      expect(wrapper.find(handle).at(0)).toIncludeText('rails')
+      expect(wrapper.find(handle).at(1)).toIncludeText('sprok')
+    })
+  })
+
+  describe('behavior', () => {
+    it('fires onRemoveClick with an id when the remove link is clicked', () => {
+      const handle = '.saved-gems__item__remove-link'
+      const spy = sinon.spy()
+      const items = [
+        {id: 'foo', name: 'rails', description: 'rails'},
+      ]
+
+      let wrapper = mount(<SavedGemsList items={items} onRemoveClick={spy}/>)
+      wrapper.find(handle).simulate('click')
+
+      expect(spy.calledWith('foo')).toBe(true)
+      expect(spy.callCount).toEqual(1)
+    })
+  })
+})
