e8c103cc9f38e416061767c727a934aefe1d0668
johny <jho406@gmail.com>
2019-09-28 23:37:43 -0400

    Add a slight improvement on the SavedGemsList test

--- src/components/SavedGemsListSpec.js
+++ src/components/SavedGemsListSpec.js
@@ -6,18 +6,12 @@ import SavedGemsList from './SavedGemsList'
 
 describe('SavedGemsList', () => {
   describe('render', () => {
-    it('renders an empty message when no items are passed', () => {
+    it('renders with a fallback empty message', () => {
       const handle = '.saved-gems__empty-notice'
       const expectedMsg = 'There are no items saved yet.'
-      const items = [
-        {id: 'foo', name: 'rails', description: 'rails'}
-      ]
 
-      let component = mount(<SavedGemsList/>)
+      const component = mount(<SavedGemsList/>)
       expect(component.find(handle)).toHaveText(expectedMsg)
-
-      component = mount(<SavedGemsList items={items}/>)
-      expect(component.find(handle)).not.toHaveText(expectedMsg)
     })
 
     it('renders a list of gems when items are passed', () => {
@@ -30,6 +24,7 @@ describe('SavedGemsList', () => {
       const component = mount(<SavedGemsList items={items}/>)
 
       expect(component).toContainMatchingElements(2, handle)
+      expect(component.find(handle)).not.toHaveText('There are no items saved yet.')
       expect(component.find(handle).at(0)).toIncludeText('rails')
       expect(component.find(handle).at(1)).toIncludeText('sprok')
     })
